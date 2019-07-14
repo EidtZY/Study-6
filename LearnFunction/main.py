26229a07eb2582cf58a4205d2c53f8fec4a48c28
EidtZY <52123321+EidtZY@users.noreply.github.com>
2019-07-14 14:51:01 +0800

    ''';

---
+++ LearnFunction/main.py
@@ -0,0 +1,11 @@
+from my_modules import*
+pri_cut_line(1)
+a = 1
+b = 2
+
+c = add(a,b)
+print(c)
+# ---------------------长方形------------------------
+pri_cut_line(2)
+
+print(cal_rect_area(15,25))
---
+++ LearnFunction/my_modules.py
@@ -0,0 +1,14 @@
+def add(a,b):
+    '这是第一个函数 add'
+    sum = a + b
+    return sum
+
+def cal_rect_area(lenth,width):
+    '计算长方形面积'
+    area = lenth * width
+    return area
+
+def pri_cut_line(sn):
+    '打印一条华丽分割线'
+    print('\n-----------------cutting line (%d)-----------------' % (sn))
+    return
